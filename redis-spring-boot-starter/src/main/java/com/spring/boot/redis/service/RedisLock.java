18e9127bb810cff1e81f1ee305021fbd17c39f79
wangbw <wangbowang1101@gmail.com>
2020-08-17 10:45:21 +0800

    修改redis分布式锁

--- redis-spring-boot-starter/src/main/java/com/spring/boot/redis/service/RedisLock.java
+++ redis-spring-boot-starter/src/main/java/com/spring/boot/redis/service/RedisLock.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
+import redis.clients.jedis.Jedis;
+
+import java.util.Collections;
 
 /**
  * redis分布式锁
@@ -20,81 +22,54 @@ public class RedisLock {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    private final static int EFAULT_RETRY_COUNT = 3;
+    private static final Long RELEASE_SUCCESS = 1L;
 
     /**
-     * 加锁
-     * @param key 锁唯一标志（不是要缓存的数据的key）
-     * @param expiredTimestamp 超时时间戳=System.currentTimeMillis()+超时时间；对应value。
-     * 默认重试三次
+     * redis分布式解锁Lua脚本
      */
-    public boolean lockRetry(String key, long expiredTimestamp){
-        return lockRetry(key,expiredTimestamp,EFAULT_RETRY_COUNT);
-    }
+    private static final String SCRIPT = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
 
+
+    private static final String LOCK_SUCCESS = "OK";
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
 
     /**
-     * 加锁
-     * @param key 锁唯一标志（不是要缓存的数据的key）
-     * @param expiredTimestamp 超时时间戳=System.currentTimeMillis()+超时时间
-     * @param retryCount redis锁获取失败重试次数
+     * 尝试获取分布式锁
+     * @param jedis Redis客户端
+     * @param lockKey 锁
+     * @param requestId 请求标识
+     * @param expireTime 超期时间
+     * @return 是否获取成功
      */
-    public boolean lockRetry(String key, long expiredTimestamp,int retryCount){
-        if(retryCount<=0){
-            //不合法时不重试
-            retryCount = 1;
-        }
-        for(int i=0;i<retryCount;i++){
-            //无自旋，重试意义不大
-            if(lock(key,expiredTimestamp)){
-                return true;
-            }
-        }
-        return false;
-    }
+    public static boolean lock(Jedis jedis, String lockKey, String requestId, int expireTime) {
 
+        String result = jedis.set(lockKey, requestId, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, expireTime);
 
-    /**
-     * 加锁
-     * 可以将value设置为锁过期时间
-     * @param key 锁唯一标志（不是要缓存的数据的key）
-     * @param expiredTimestamp 超时时间戳=System.currentTimeMillis()+超时时间
-     */
-    public boolean lock(String key, long expiredTimestamp){
-        String value = String.valueOf(expiredTimestamp);
-
-        //如果设置成功，则说明当前锁标志无人占用，当前线程将占用该锁
-        if(redisTemplate.opsForValue().setIfAbsent(key,value)){
+        if (LOCK_SUCCESS.equals(result)) {
             return true;
         }
+        return false;
 
-        //判断锁超时,防止死锁
-        //当前锁的value
-        String currentValue = (String)redisTemplate.opsForValue().get(key);
-        //如果锁过期
-        if(!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()){
-            //获取上一个锁的时间value
-            String oldValue = (String)redisTemplate.opsForValue().getAndSet(key,value);
-            if(!StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue) ){
-                return true;
-            }
+    }
+
+    /**
+     * 释放分布式锁
+     * @param jedis Redis客户端
+     * @param lockKey 锁
+     * @param requestId 请求标识
+     * @return 是否释放成功
+     */
+    public static boolean unLock(Jedis jedis, String lockKey, String requestId) {
+
+        /**eval命令执行Lua代码的时候，Lua代码将被当成一个命令去执行，并且直到eval命令执行完成，Redis才会执行其他命令。*/
+        Object result = jedis.eval(SCRIPT, Collections.singletonList(lockKey), Collections.singletonList(requestId));
+
+        if (RELEASE_SUCCESS.equals(result)) {
+            return true;
         }
         return false;
+
     }
-    /**
-     * 解锁
-     * @param key 加锁时用的key
-     * @param value 加锁时设置的value
-     */
-    public void unlock(String key,String value){
-        try {
-            String currentValue =  (String) redisTemplate.opsForValue().get(key);
-            if(!StringUtils.isEmpty(currentValue) && currentValue.equals(value) ){
-                //删除key
-                redisTemplate.opsForValue().getOperations().delete(key);
-            }
-        } catch (Exception e) {
-            log.error("【Redis分布式锁】解锁出现异常了，{}",e);
-        }
-    }
+
 }
